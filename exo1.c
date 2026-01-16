e8013d70bc6922ec1fc6319b8b421f19ae98dac3
Dylan INNOU <dylan.innou@epita.fr>
2026-01-17 00:37:21 +0100

    feat: exercise 1 - pointers

---
+++ exo1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// FIXME: Some code was deleted here...
+
+int main(void)
+{
+    int a = 5;
+    int old_a = a;
+
+    // FIXME: Make a function call with a and square it's value.
+    
+    printf("%d changed to %d\n", old_a, a);
+}
