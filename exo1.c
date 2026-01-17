d0aac0e0858d676d447f603583dd3a236946c309
Florian Dubreuil <florian.dubreuil@epita.fr>
2026-01-17 15:00:27 +0100

    a

--- exo1.c
+++ exo1.c
@@ -2,11 +2,19 @@
 
 // FIXME: Some code was deleted here...
 
+void change(int *x, int y)
+{
+	*x = y;
+
+}
+
+
+
 int main(void)
 {
     int a = 5;
     int old_a = a;
-
+    change(&a, 2);
     // FIXME: Make a function call with a and square it's value.
     
     printf("%d changed to %d\n", old_a, a);
