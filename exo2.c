9f1505e025cd740f5f1b865b2460a8d2741a3f37
Dylan INNOU <dylan.innou@epita.fr>
2026-01-17 15:02:07 +0100

    feat: new exercise

---
+++ exo2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * This function should swap the value of a and b.
+ * @param a random integer pointer
+ * @param b another random integer pointer
+ */
+void swap(int* a, int* b)
+{
+    // FIXME: Some code was deleted here...
+}
+
+int main(void)
+{
+    int a = 4;
+    int b = -2;
+
+    int old_a = a;
+    int old_b = b;
+
+    printf("%d changed to %d ; and %d changed to %d\n", old_a, a, old_b, b);
+
+    return 0;
+}
