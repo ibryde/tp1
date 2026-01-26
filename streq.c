f4c5c623f1a24ffe2def7a8b6538dd07facc7e9b
Dylan INNOU <dylan.innou@epita.fr>
2026-01-26 15:01:03 +0100

    fix(streq.c): resolve conflict

--- streq.c
+++ streq.c
@@ -1,14 +1,8 @@
 // #include <stddef.h>
 #include <stdio.h>
-<<<<<<< HEAD
-#include <string.h>
-int my_streq(const char *s1, const char *s2)
-=======
-#include <stdbool.h>
 #include <string.h>
 
-int streq(const char *s1, const char *s2)
->>>>>>> f7d092256cf0e8a9be2f0541084c15220210f3f4
+int my_streq(const char *s1, const char *s2)
 {
 	for (int i = 0; *s1 != '\0' || *s2 != '\0'; i++)
 	{
@@ -35,14 +29,6 @@ int my_strlen(const char *var)
 
 int main(void)
 {
-<<<<<<< HEAD
 	printf("%d\n", my_streq("vvv", "vv"));
 	return 0;
-
-=======
-    char text1[] = {'H', 'e', 'l', 'l', 'o', '!', 0};
-    char* text2 = "Hello!";
-
-	printf("%d\n", streq(text1,text2));
->>>>>>> f7d092256cf0e8a9be2f0541084c15220210f3f4
 }
